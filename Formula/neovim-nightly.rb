dce02254ddb9660604b0bd2bc228ca11c5ab5f92
vhqkze <zqz0601@gmail.com>
2025-07-23 19:46:24 +0800

    feat: add neovim-nightly formula

---
+++ Formula/neovim-nightly.rb
@@ -0,0 +1,32 @@
+class NeovimNightly < Formula
+  desc "Ambitious Vim-fork focused on extensibility and usability"
+  homepage "https://neovim.io/"
+  version "v0.12.0-dev-888+g95dfb063da"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz"
+    sha256 "1e6add44cfb6ee520cf9071c51213ef9dfb74cfb0529ec5443cb066ad7defe64"
+  else
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz"
+    sha256 "5502548aee138a359a7566ce36915804fb268dd1683621313c5098d874770cc8"
+  end
+
+  depends_on :macos
+
+  conflicts_with "neovim", because: "both install `nvim` binaries"
+
+  def install
+    prefix.install "bin", "share", "lib"
+  end
+
+  def caveats
+    <<~EOS
+      This is a nightly build of Neovim.
+      It may be unstable and subject to frequent changes.
+    EOS
+  end
+
+  test do
+    assert_match "NVIM #{version}", shell_output("#{bin}/nvim --version")
+  end
+end
